7652bc551d72e8f4a2e7786d5115e7b86111136e
Elvis Chege <96030189+elviscgn@users.noreply.github.com>
2026-05-03 08:14:08 +0200

    feat: timescale and postgres ported

--- internal/db/migrate.go
+++ internal/db/migrate.go
@@ -1,1 +1,12 @@
 package db
+
+import "github.com/uwatu/uwatu-core/internal/config"
+
+func RunMigrations() {
+	if Pool == nil {
+		config.LogInfo("DB", "No pool, skipping migrations")
+		return
+	}
+	// TODO: Run SQL files from /migrations directory
+	config.LogInfo("DB", "Migrations skipped (not implemented)")
+}
